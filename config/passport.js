64bb0b5dda8cce988e344e959bddd2a1cdd8dc43
nicodenelliGH <ndm9412@gmail.com>
2023-03-24 12:44:05 -0400

    index view setup

--- config/passport.js
+++ config/passport.js
@@ -1,25 +1,45 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 //Require your User Model here!
+const UserModel = require('../models/user');
+
 
 // configuring Passport!
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_SECRET,
     callbackURL: process.env.GOOGLE_CALLBACK
-  },
-  function(accessToken, refreshToken, profile, cb) {
+  }, async function(accessToken, refreshToken, profile, cb) {
     // a user has logged in via OAuth!
     // refer to the lesson plan from earlier today in order to set this up
+    try {
+      let user = await UserModel.findOne({googleId: profile.id});
+      if(user) return cb(null, user);
+      user = await UserModel.create({
+        name: profile.displayName,
+        googleId: profile.id,
+        email: profile.emails[0].value,
+        avatar: profile.photos[0].value
+      })
+      cb(null, user)
 
-  }
-));
+    } catch(err){
+      cb(err)
+    }
+  }));
 
 passport.serializeUser(function(user, cb) {
-  cb(null, user.id);
+  cb(null, user._id);
 });
 
-passport.deserializeUser(function(id, cb) {
+passport.deserializeUser(function(userId, cb) {
+  UserModel.findbyId(userId)
+           .then(function(userDoc){
+            cb(null, userDoc);
+
+           }).catch(err => {
+            cb(err)
+           })
 
   // Find your User, using your model, and then call done(err, whateverYourUserIsCalled)
   // When you call this done function passport assigns the user document to req.user, which will 
--- models/user.js
+++ models/user.js
@@ -1,3 +1,17 @@
 const mongoose = require('mongoose');
 
 // Create your User Model
+const userSchema = new mongoose.Schema(
+    {
+      name: String,
+      avatar: String,
+      email: String,
+      googleId: {
+        type: String,
+        required: true,
+      }
+    },
+    
+  );
+  
+  module.exports = mongoose.model("User", userSchema);
