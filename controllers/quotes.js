040d4a1689717c5df045393353d1314f69873960
nicodenelliGH <ndm9412@gmail.com>
2023-03-28 15:39:29 -0400

    adding delete method

--- controllers/quotes.js
+++ controllers/quotes.js
@@ -9,9 +9,28 @@ module.exports = {
 function deleteQuote(req, res){
 	// tell the model to delete the quote of the id 
 	// req.params.id represents the id coming from the form on the client
-	CharacterModel.deleteOne(req.params.id);
-	res.redirect('/supers')
-}
+	CharacterModel.findOne({
+		"quote._id": req.params.id,
+		"quote.userId": req.user._id,
+	  }).then(function (characterDoc) {
+		// if we don't find a match, characterDoc would be undefined
+		if(!characterDoc) return res.redirect('/supers');
+	
+		// remove is method on mongoose subdocuments
+		// review is a subdocument
+		// its embedded in the movies, One movie has many rev iews
+		characterDoc.reviews.remove(req.params.id);  // <- mutated a document!
+		// mongodb doesn't know we removed the review, 
+		// so we have to call save on the movieDoc to tell mongodb
+		characterDoc.save().then(function(){
+			res.redirect(`/supers/${characterDoc._id}`); // go back to the character change where the delete quote form was
+		})
+	
+	
+	  }).catch(err => {
+		res.send(err)
+	  })
+	}
 
 function create(req, res){
 	console.log(req.body)
