dd2a6c25e32a13ca369478a41de2c5920799a9ae
nicodenelliGH <ndm9412@gmail.com>
2023-03-30 16:04:23 -0400

    updating deployment

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const passport = require('passport');
 const methodOverride = require('method-override');
+const MongoStore = require('connect-mongo');
 
 const indexRouter = require('./routes/index');
 const charactersRouter = require('./routes/characters');
@@ -37,6 +38,9 @@ app.use(cookieParser());
 app.use(methodOverride('_method'));
 // mount the session middleware
 app.use(session({
+  store: MongoStore.create({
+    mongoUrl: process.env.DATABASE_URL
+  }),
   secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true
